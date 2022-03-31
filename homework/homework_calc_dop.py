91b9a963b8289c0a71bdf7925d24086573332651
Alexandr Bolotin <alexbolotin89@gmail.com>
2022-03-31 14:39:33 +0300

    1

--- homework/homework_calc_dop.py
+++ homework/homework_calc_dop.py
@@ -10,32 +10,35 @@ n = 0
 
 while s!='exit':
     s = input("Введите данные через пробел: ").split()
-    if s =='exit':
-        print('Спасибо, до скорых встреч!')
-    else:    
-        if s[1] in ss:
-            x = float(s[0])
-            y = float(s[2])
-            n += 1 
-            if s[1] =='+':
-                print("Результат:\n",s[0],s[1],s[2],'=', (x+y),'\n', decimal2text( (x+y), places=1,int_units=int_units,exp_units=exp_units))
-                print('\nКоличество произведенных операций:', n)
-            elif s[1] =='-':
-                print("Результат:\n",s[0],s[1],s[2],'=', (x-y),'\n',decimal2text( (x-y), places=1,int_units=int_units,exp_units=exp_units))
-                print('\nКоличество произведенных операций:', n)
-            elif s[1] =='*':
-                print("Результат:\n",s[0],s[1],s[2],'=', (x*y),'\n',decimal2text( (x*y), places=1,int_units=int_units,exp_units=exp_units))
-                print('\nКоличество произведенных операций:', n)
-            elif s[1] =='/':
-                if y == 0:
-                    print('\nДеление на 0 запрещено')
+    try:
+        if s[0] =='exit':
+            print('Спасибо, до скорых встреч!')
+            break
+        else:    
+                if s[1] in ss:
+                    x = float(s[0])
+                    y = float(s[2])
+                    n += 1 
+                    if s[1] =='+':
+                        print("Результат:\n",s[0],s[1],s[2],'=', (x+y),'\n', decimal2text( (x+y), places=1,int_units=int_units,exp_units=exp_units))
+                        print('\nКоличество произведенных операций:', n)
+                    elif s[1] =='-':
+                        print("Результат:\n",s[0],s[1],s[2],'=', (x-y),'\n',decimal2text( (x-y), places=1,int_units=int_units,exp_units=exp_units))
+                        print('\nКоличество произведенных операций:', n)
+                    elif s[1] =='*':
+                        print("Результат:\n",s[0],s[1],s[2],'=', (x*y),'\n',decimal2text( (x*y), places=1,int_units=int_units,exp_units=exp_units))
+                        print('\nКоличество произведенных операций:', n)
+                    elif s[1] =='/':
+                        if y == 0:
+                            print('\nДеление на 0 запрещено')
+                        else:
+                            print("Результат:\n",s[0],s[1],s[2],'=', (x/y),'\n',decimal2text( (x/y), places=1,int_units=int_units,exp_units=exp_units))
+                            print('\nКоличество произведенных операций:', n)
+                    elif s[1] =='**':
+                        print("Результат:\n",s[0],s[1],s[2],'=', (x**y),'\n',decimal2text( (x**y), places=1,int_units=int_units,exp_units=exp_units))
+                        print('\nКоличество произведенных операций:', n)
                 else:
-                    print("Результат:\n",s[0],s[1],s[2],'=', (x/y),'\n',decimal2text( (x/y), places=1,int_units=int_units,exp_units=exp_units))
-                    print('\nКоличество произведенных операций:', n)
-            elif s[1] =='**':
-                print("Результат:\n",s[0],s[1],s[2],'=', (x**y),'\n',decimal2text( (x**y), places=1,int_units=int_units,exp_units=exp_units))
-                print('\nКоличество произведенных операций:', n)
-        else:
-            print('Введён неверный арифметический знак, введите согласно списку : +,-,*,/,**')
-
+                        print('Введён неверный арифметический знак, введите согласно списку : +,-,*,/,**')
+    except:
+        print("Неверный ввод данных")
 
--- homework/hpmework_1_calc.py
+++
@@ -1,9 +0,0 @@
-
-s = input("Знак (+,-,*,/): ")
-x = float(input("x="))
-y = float(input("y="))
-
-w=eval(str(x)+str(s)+str(y))
-
-print(w)
-
