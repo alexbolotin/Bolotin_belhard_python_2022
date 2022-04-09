2d2a4d76e60067b57cfff323fd2ad4f731ac34f3
Alexandr Bolotin <alexbolotin89@gmail.com>
2022-04-09 14:35:59 +0300

    1

--- homework/dz06/yes_or_no.py
+++ homework/dz06/yes_or_no.py
@@ -3,14 +3,12 @@ def yes_or_no(s):
     for i in s: 
         if i in spisok:
             spisok[i] += 1
+            print(f'Элемент {i} Yes')
         else:
             spisok[i] = 1
-    print(spisok)
-    for i in s:
-        if spisok[i] !=1: 
-            print(f'Элемент {i} Yes')  
-        else:
-            print(f'Элемент {i} No')  
+            print(f'Элемент {i} No')
+
+
 #s = input('Введите список: ')
 s = (1,10,2,3,'q',4,5,1,'q','w','q',1,'q',1,1,'q',1,1,'q',1,1)
 yes_or_no(s)
