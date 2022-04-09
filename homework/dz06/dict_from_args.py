aca8a75a685113c3bd9262800f65fee0ccb3c165
Alexandr Bolotin <alexbolotin89@gmail.com>
2022-04-09 17:15:35 +0300

    finish

--- homework/dz06/dict_from_args.py
+++ homework/dz06/dict_from_args.py
@@ -1,33 +1,33 @@
-def sum_of_args(s):
-    s1 = 0
-    for i in s.split(','):
-        s1 +=int(i)
-    return s1
+def sum_of_args(args):
+    sum = 0
+    for i in args:
+        sum +=int(i)
+    return sum
 
-def kwargs_max_len(i):
-    global max_len
-    i_len = 0
-    if type(i) == str:
-            i_len = len(i)
-            if max_len < i_len:
-                max_len = i_len
-    return max_len      
+def kwargs_max_len(kwargs):
+        max_len = 0
+        i_len = 0
+        for i in kwargs:
+                i_len = len(i)
+                if max_len < i_len:
+                        max_len = i_len
+        return max_len      
 
-max_len = 0
-sum = 0
-s = '1,2,3,400,10'
-#s = '1,2,3',[1,2,3],(1,2,3)
-#s = '1,2,3,qwe'
-#s = input('Введите список: ')
 
-try:
-    for i in s.split(','): 
-        kwargs_max_len(i)
-    print('kwargs_max_len = ', max_len)
-    if all(i.isdigit() for i in s.split(',')):
-        print('Сумма = ', sum_of_args(s))
-    else:
-        print('Все аргументы - ключевые слова должны быть строками')
-    
-except:
-    print('Все аргументы - ключевые слова должны быть строками')
+def dict_from_args(*args, **kwargs):
+        result = {}
+        if all(type(i) is int for i in args):
+                # print('Сумма = ', sum_of_args(args))
+                result['sum_of_args'] = sum_of_args(args)       
+        else:
+                print('Все аргументы - должны быть целоочисленными')
+
+        if all([type(i) is str for i in kwargs.values()]):
+                # print('Максимальная длинна = ',kwargs_max_len(kwargs.values()))
+                result['kwargs_max_len'] = kwargs_max_len(kwargs.values())
+        else:
+                print('Все аргументы - ключевые слова должны быть строками')
+        return result
+
+
+print(dict_from_args(1,2,3,4,5,q = '12',w = '12345',e = 'qwe',r = '1234'))
--- homework/dz06/logging_decorator.py
+++ homework/dz06/logging_decorator.py
@@ -1,19 +1,20 @@
-def decorator(funct):
+def decorator(funct,*args,**kwargs):
     def wrapper(*args,**kwargs):
-        print(f'Выполняем функцию {funct.__name__}')
+        print(f'Выполняем функцию {funct.__name__} с args {args} и kwargs {kwargs}')
         funct(name)
         print(f'Выполнено {funct.__name__}')
     return wrapper
 
 @decorator
-def hello1(name):
+def hello1(name,*args,**kwargs):
     print(f'Привет {name}')
 
 
 name = 'Alex'
+age = 12
 #name = input('Введите своё имя: ')
 
-hello1(name)
+hello1(name,age,n = 1,q = 2)
 
 
 
