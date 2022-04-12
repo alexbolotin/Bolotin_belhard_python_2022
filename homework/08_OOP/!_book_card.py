32d53c2f61f4a2e7a949fda972e024371e1bcb5b
Alexandr Bolotin <alexbolotin89@gmail.com>
2022-04-12 16:59:54 +0300

    1

--- homework/08_OOP/!_book_card.py
+++ homework/08_OOP/!_book_card.py
@@ -1,4 +1,7 @@
+from datetime import date
+
 class BookCard():
+    
     def __init__(self,author:str, title:str, year:int):
         self.__author = author
         self.__title = title
@@ -12,8 +15,38 @@ class BookCard():
         return self.__author
 
     @property
+    def author(self):
+        return self.__author
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
     def year(self):
         return self.__year
+
+    @author.setter
+    def author(self,author):
+        if type(author) != str:
+            raise Warning (ValueError, 'ошибка данных')
+        else: 
+            self.__author = author
+
+    @title.setter
+    def title(self,title):
+        if type(title) != str:
+            raise Warning (ValueError, 'ошибка данных')
+        else: 
+            self.__title = title
+
+    @year.setter
+    def year(self,year):
+        CURRENT_YEAR = date.today().year
+        if (type(year) != int or year < 0 or year > CURRENT_YEAR):
+            raise Warning (ValueError, 'ошибка данных')
+        else: 
+            self.__year = year
 
 class Books:
     def __init__(self, l):
@@ -25,10 +58,12 @@ class Books:
     def sort(self,q):
         return sorted(books1, key =  q)
 
-
 book1 = BookCard('moby', 'rasskaz', 1860)
 book2 = BookCard('esenin', 'stihi', 1920)
 book3 = BookCard('pushkin', 'skazka', 1830)
+
+book1.author = 'Moby'
+book1.year = 1861
 
 book1.get_info()
 book2.get_info()
